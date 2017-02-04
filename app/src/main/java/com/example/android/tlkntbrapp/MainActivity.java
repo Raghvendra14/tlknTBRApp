514b5cf5f026c5e9f100b71f83b56386042f0b63
Raghvendra <raghvendrasrivastava14@gmail.com>
2017-02-04 14:01:21 +0530

    bug fix

--- app/src/main/java/com/example/android/tlkntbrapp/MainActivity.java
+++ app/src/main/java/com/example/android/tlkntbrapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.tlkntbrapp;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = this;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             View shadowView = findViewById(R.id.shadow_view);
             shadowView.setVisibility(View.VISIBLE);
         }
+
+        CircleImageView userIcon = (CircleImageView) findViewById(R.id.user_icon);
+        userIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, R.string.user_content, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageView dialerIcon = (ImageView) findViewById(R.id.dialer_icon);
+        dialerIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, R.string.dialer, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
